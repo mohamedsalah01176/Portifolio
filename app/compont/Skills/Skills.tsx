c4547777cf238f4e841249dd1ef676c7a5264fab
mohamed salah <m48162698@gmial.com>
2025-05-31 21:36:43 +0300

    update animation

--- app/compont/Skills/Skills.tsx
+++ app/compont/Skills/Skills.tsx
@@ -11,52 +11,52 @@ export default function Skills() {
         <h1 className='uppercase text-yellow-500 text-5xl font-semibold text-center' data-aos="fade-up">Skills</h1>
         <h1  className='mt-10 mb-5 text-2xl font-semibold ' data-aos="fade-up" >Front End</h1>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-x-5 gap-y-3' >
-          <div className='flex  gap-2 flex-col' data-aos="fade-right">
-            <SklillesItem title={"HTML"} level={"95%" } />
-            <SklillesItem title={"CSS"} level={"85%" } />
-            <SklillesItem title={"JAVASCRIPT"} level={"90%" }/>
-            <SklillesItem title={"Typecscript"} level={" 90%" }/>
-            <SklillesItem title={"Tailwind"} level={"95%" }/>
-            <SklillesItem title={"Material UI"} level={"95%" }/>
+          <div className='flex  gap-2 flex-col' >
+            <SklillesItem state="down" title={"HTML"} level={"95%" }  />
+            <SklillesItem state="right" title={"CSS"} level={"85%" }  />
+            <SklillesItem state="right" title={"JAVASCRIPT"} level={"90%" } />
+            <SklillesItem state="right" title={"Typecscript"} level={" 90%" } />
+            <SklillesItem state="right" title={"Tailwind"} level={"95%" } />
+            <SklillesItem state="up" title={"Material UI"} level={"95%" } />
           </div>
-          <div className='flex  gap-2 flex-col' data-aos="fade-left">
-            <SklillesItem title={"SASS"} level={"90%" }/>
-            <SklillesItem title={"Bootstrap"} level={"80%" }/>
-            <SklillesItem title={"React JS"} level={"95%" }/>
-            <SklillesItem title={"Angulat TS"} level={"95%" }/>
-            <SklillesItem title={"Next JS"} level={"95%" }/>
-            <SklillesItem title={"Redux Toolkit"} level={"95%" }/>
+          <div className='flex  gap-2 flex-col' >
+            <SklillesItem state="down" title={"SASS"} level={"90%" } />
+            <SklillesItem state="left" title={"Bootstrap"} level={"80%" } />
+            <SklillesItem state="left" title={"React JS"} level={"95%" } />
+            <SklillesItem state="left" title={"Angulat TS"} level={"95%" } />
+            <SklillesItem state="left" title={"Next JS"} level={"95%" } />
+            <SklillesItem state="up" title={"Redux Toolkit"} level={"95%" } />
           </div>
         </div>
         <h1 className='mt-10 mb-5 text-2xl font-semibold ' data-aos="fade-up">Back End</h1>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-x-5 gap-y-3'>
           <div className='flex  gap-2 flex-col' data-aos="fade-right">
-            <SklillesItem title={"Node JS"} level={"90%" }/>
-            <SklillesItem title={"Express"} level={"95%" }/>
+            <SklillesItem state="down" title={"Node JS"} level={"90%" }/>
+            <SklillesItem state="up" title={"Express"} level={"95%" }/>
           </div>
           <div className='flex  gap-2 flex-col' data-aos="fade-left">
-            <SklillesItem title={"Mongo DB"} level={"90%" }/>
-            <SklillesItem title={"Nest JS"} level={"85%" }/>
+            <SklillesItem state="down" title={"Mongo DB"} level={"90%" }/>
+            <SklillesItem state="up" title={"Nest JS"} level={"85%" }/>
           </div>
         </div>
         <h1 className='mt-10 mb-5 text-2xl font-semibold ' data-aos="fade-up">Tools</h1>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-x-5 gap-y-3'>
           <div className='flex  gap-2 flex-col' data-aos="fade-right">
-            <SklillesItem title={"Git"} level={"90%" }/>
-            <SklillesItem title={"Postman"} level={"95%" }/>
+            <SklillesItem state="down" title={"Git"} level={"90%" }/>
+            <SklillesItem state="up" title={"Postman"} level={"95%" }/>
           </div>
           <div className='flex  gap-2 flex-col' data-aos="fade-left">
-            <SklillesItem title={"Scurm"} level={"80%" }/>
+            <SklillesItem state="down" title={"Scurm"} level={"80%" }/>
           </div>
         </div>
         <h1 className='mt-10 mb-5 text-2xl font-semibold ' data-aos="fade-up">Other</h1>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-x-5 gap-y-3'>
           <div className='flex  gap-2 flex-col' data-aos="fade-right">
-            <SklillesItem title={"Data Structure"} level={"80%" }/>
-            <SklillesItem title={"C"} level={"80%" }/>
+            <SklillesItem state="down" title={"Data Structure"} level={"80%" }/>
+            <SklillesItem state="up" title={"C"} level={"80%" }/>
           </div>
           <div className='flex  gap-2 flex-col' data-aos="fade-left">
-            <SklillesItem title={"C++"} level={"90%" }/>
+            <SklillesItem state="down" title={"C++"} level={"90%" }/>
           </div>
         </div>
       </div>
--- app/compont/Skills/SklillesItem.tsx
+++ app/compont/Skills/SklillesItem.tsx
@@ -4,16 +4,18 @@ import React from 'react'
 
 interface prop{
     title:string,
-    level:string
+    level:string,
+    state:string,
 }
 
-export default function SklillesItem({title,level}:prop) {
+export default function SklillesItem({title,level,state}:prop) {
     
-  return (
-    <div className='bg-gray-800 p-5 relative' >
-        <h1 className='text-lg font-semibold'>{title}</h1>
-        <div className={`  absolute bottom-0 left-0 h-1 bg-[#55e6a5] px-[${level}%]`} style={{width:level}}></div>
-        <div className='absolute top-4 right-2 rounded-full p-2 bg-yellow-500'>{level}</div>
-    </div>
-  )
+    return (
+      <div className='bg-gray-800 p-5 relative' data-aos={`fade-${state}`} >
+          <h1 className='text-lg font-semibold'>{title}</h1>
+          <div className={`  absolute bottom-0 left-0 h-1 bg-[#55e6a5] px-[${level}%]`} style={{width:level}}></div>
+          <div className='absolute top-4 right-2 rounded-full p-2 bg-yellow-500'>{level}</div>
+      </div>
+    )
+
 }
